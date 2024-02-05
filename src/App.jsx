23c6243b0de857f2a8729bb672ce8e111c9b62f6
Jaumemasmorla <masjaume0@gmail.com>
2024-02-05 21:54:18 +0100

    style and fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,93 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import { useState, useEffect,useId } from 'react';
 import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import React from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+
+const fetchSchedule = async () => {
+  const url = 'https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php';
+  const response = await fetch(url);
+  if (!response.ok) throw response;
+  return await response.json();
+};
+
+const schedule = {
+  "title": "CS Courses for 2018-2019",
+  "courses": {
+    "F101" : {
+      "id" : "F101",
+      "meets" : "MWF 11:00-11:50",
+      "title" : "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110" : {
+      "id" : "F110",
+      "meets" : "MWF 10:00-10:50",
+      "title" : "Intro Programming for non-majors"
+    },
+    "S313" : {
+      "id" : "S313",
+      "meets" : "TuTh 15:30-16:50",
+      "title" : "Tangible Interaction Design and Learning"
+    },
+    "S314" : {
+      "id" : "S314",
+      "meets" : "TuTh 9:30-10:50",
+      "title" : "Tech & Human Interaction"
+    }
+  }
+};
+
+const Banner = ({ title }) => (
+  <h1>{ title }</h1>
+);
+
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const getCourseTerm = courses => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+);
+
+const Course = ({ course }) => (
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
+  </div>
+);
+
+const CourseList = ({ courses }) => (
+  <div className="course-list">
+  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+  </div>
+);
+
+const Main = () =>  {
+  const { schedule: data, isLoading, error } = useQuery({
+    queryKey: ['schedule'],
+    queryFn: fetchSchedule
+  });
+  
+  if (error) return <h1>{error}</h1>;
+  if (isLoading) return <h1>Loading the schedule...</h1>
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
     </div>
   );
 };
 
-export default App;
-console.log(App);
+const queryClient = new QueryClient();
+
+ export const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Main />
+  </QueryClientProvider>
+);
+

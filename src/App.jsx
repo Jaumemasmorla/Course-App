22aa86f3040b01f543d5cd8dbd9bd9b71286a01c
Jaumemasmorla <masjaume0@gmail.com>
2024-02-06 18:34:48 +0100

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import React from 'react';
+import { Banner } from './components/banner';
+import { CourseList } from './components/courses';
+import { TermButton } from './components/terms';
+import { TermSelector } from './components/terms';
+import { Course } from './components/courses';
+import { addScheduleTimes } from './utitlities/functions';
 
 
 const fetchSchedule = async () => {
 
   /*en el fetch de la URL es donde empiezan los problemas 
   ya que no sale la lista completa*/
-  const url = 'https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php';
-  const response = await fetch(url);
-  if (!response.ok) throw response;
-  return await response.json();
-};
-
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "id" : "F101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "id" : "F110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "id" : "S313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "id" : "S314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
-
-const Banner = ({ title }) => (
-  <h1>{ title }</h1>
-);
-
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
-
-
-
-/*getcourseterm modificado para sacar nulls*/
-const getCourseTerm = course => (
-  course && course.id ? terms[course.id.charAt(0)] : ''
-);
-/*getcoursenumber modificado para sacar nulls*/
-const getCourseNumber = course => (
-  course && course.id ? course.id.slice(1, 4) : ''
-);
-
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-      <div className="card-text">{ course.title }</div>
-    </div>
-  </div>
-);
-
-/*courseList modificado*/
-const CourseList = ({ courses }) => {
+ const url = "https://firebasestorage.googleapis.com/v0/b/ubiqum-academy.appspot.com/o/MobileWeb%2Fschedule.json?alt=media&token=706644eb-f870-4065-8735-cbdf48204ad1"
+  const response = await fetch('https://firebasestorage.googleapis.com/v0/b/ubiqum-academy.appspot.com/o/MobileWeb%2Fschedule.json?alt=media&token=706644eb-f870-4065-8735-cbdf48204ad1');
   
-  const [term, setTerm] = useState('Fall');
-/*los errores vuelven con el termCourses, setTerm y useState*/
-  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
-  return (
-  <>
-    <TermSelector term={term} setTerm={setTerm}/>
-    <div className="course-list">
-    { termCourses.map(course => <Course key={course.id} course={ course } />) }
-    </div>
-  </>);
-}
+  if (!response.ok) throw response;
+  return addScheduleTimes(await response.json());
+};
 
-
-/*term button modificado*/
-const TermButton = ({term, setTerm, checked}) => (
-  <>
-    <input type="radio"id={term} className="btn-check" checked = {checked} autoComplete="off" onChange={() => setTerm(term)}/>
-    <label className="btn btn-success m-1 p-2" htmlFor={term}>
-    { term }
-    </label>
-  </>
-);
-
-/*term selector modificado*/
-const TermSelector = ({term, setTerm}) => (
-  <div className="btn-group">
-  { 
-    Object.values(terms)
-      .map(value => (<TermButton key={value} term={value} setTerm ={setTerm}checked={value === term}/>))
-  }
-  </div>
-);
-/*main, data modificado*/
 const Main = () =>  {
   const { data, isLoading, error } = useQuery({
     queryKey: ['schedule'],
     queryFn: fetchSchedule
   });
-  
-  console.log(data);
 
+
+  
   if (error) return <h1>{error}</h1>;
-  if (isLoading || !data) return <h1>Loading the schedule...</h1>
+  if (isLoading) return <h1>Loading the schedule...</h1>
 
   return (
     <div className="container">
---
+++ src/components/courses.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import { hasConflict } from "../utitlities/functions";
+import { toggle } from "../utitlities/functions";
+import { getCourseTerm } from "../utitlities/functions";
+
+import { TermSelector } from '../components/terms';
+
+
+export const Course = ({ course, selected, setSelected }) =>{
+    const isSelected = selected.includes(course);
+    const isDisabled = !isSelected && hasConflict(course, selected);
+    const style = {
+      backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
+    };
+    return(
+    <div className="card m-1 p-2"
+    style={style}
+      onClick = {isDisabled ? null : () => setSelected(toggle(course, selected))}>
+      <div className="card-body">
+        <div className="card-title">{ getCourseTerm(course) }</div>
+        <div className="card-text">{ course.title }</div>
+        <div className="card-text">{ course.meets }</div>
+      </div>
+    </div>
+    )
+  };
+  
+  export const CourseList = ({ courses }) => {
+    const [term, setTerm] = useState('Fall');
+    const [selected, setSelected] =useState([]);
+    const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+    
+    return (
+      <>
+        <TermSelector term={term} setTerm={setTerm} />
+        <div className="course-list">
+        { termCourses.map(course => <Course key={course.id} course={ course } 
+        selected={selected} setSelected={setSelected}
+        />) }
+        </div>
+     </>
+    );
+  };

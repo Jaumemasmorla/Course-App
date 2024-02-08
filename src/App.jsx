16865153f68d8144a37fde13dec6f1ffe9f13312
Jaumemasmorla <masjaume0@gmail.com>
2024-02-08 09:49:32 +0100

    read from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import React from 'react';
 import { Banner } from './components/banner';
 import { CourseList } from './components/courses';
+import { Course } from './components/courses';
 import { TermButton } from './components/terms';
 import { TermSelector } from './components/terms';
-import { Course } from './components/courses';
+
 import { addScheduleTimes } from './utitlities/functions';
+import {useData} from './utitlities/firebase';
+
+
 
 
 const fetchSchedule = async () => {
 
-  /*en el fetch de la URL es donde empiezan los problemas 
-  ya que no sale la lista completa*/
+  
  const url = "https://firebasestorage.googleapis.com/v0/b/ubiqum-academy.appspot.com/o/MobileWeb%2Fschedule.json?alt=media&token=706644eb-f870-4065-8735-cbdf48204ad1"
   const response = await fetch('https://firebasestorage.googleapis.com/v0/b/ubiqum-academy.appspot.com/o/MobileWeb%2Fschedule.json?alt=media&token=706644eb-f870-4065-8735-cbdf48204ad1');
   
   if (!response.ok) throw response;
   return addScheduleTimes(await response.json());
 };
-
+/*despues del useData notendria que ir '/courses'?*/
 const Main = () =>  {
+  
   const { data, isLoading, error } = useQuery({
     queryKey: ['schedule'],
     queryFn: fetchSchedule
@@ -39,7 +43,6 @@ const Main = () =>  {
     </div>
   );
 };
-
 const queryClient = new QueryClient();
 
  export const App = () => (
--- src/utitlities/firebase.jsx
+++ src/utitlities/firebase.jsx
@@ -1,15 +1,40 @@
-import { initializeApp } from 'firebase/app';
+
+
+import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { useDatabaseValue } from "@react-query-firebase/database";
 import { getDatabase, onValue, ref, set } from 'firebase/database';
 
+
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...",
-  projectId: "...",
-  storageBucket: "....",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDPiIqWs7A1gs4ituLrwcK_BgrjqvKOEKc",
+  authDomain: "courses-app-9f186.firebaseapp.com",
+  databaseURL: "https://courses-app-9f186-default-rtdb.firebaseio.com",
+  projectId: "courses-app-9f186",
+  storageBucket: "courses-app-9f186.appspot.com",
+  messagingSenderId: "490447294185",
+  appId: "1:490447294185:web:1b443a7fe6ca13a0242a78",
+  measurementId: "G-9YEWWD2NL7"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const firebase = initializeApp(firebaseConfig);
 const database = getDatabase(firebase);
+
+
+
+
+
+
+export const useData = (path, transform) => {
+    const { data, isLoading, error } = useDatabaseValue([path], ref(database, path), { subscribe: true });
+    const value = (!isLoading && !error && transform) ? transform(data) : data;
+  
+    return [ value, isLoading, error ];
+  
+ 
+};
